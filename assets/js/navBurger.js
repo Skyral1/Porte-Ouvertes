eac2c1c3fddc3aca77eba68e7687a3a7d2e94a2c
Thibault Soquette <thibault.soquette.pro@gmail.com>
2023-04-16 17:36:34 +0200

    Fix : Bug mobile
    Signed-off-by: Thibault Soquette <thibault.soquette.pro@gmail.com>

--- assets/js/navBurger.js
+++ assets/js/navBurger.js
@@ -1,5 +1,5 @@
 function myFunction() {
-	var x = document.getElementById("nav_burger");
+	var x = document.getElementById("burger-action");
 	if (x.style.display === "block") {
 		x.style.display = "none";
 	} else {
